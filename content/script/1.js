cb93697b049cd5a7d1e6b230ffdc1c161d196f6d
zdrpepe <petrool12@yandex.ru>
2018-07-18 17:58:57 +0700

    d

--- content/script/1.js
+++ content/script/1.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var x = document.getElementsByClassName("h1");
+  var x = document.getElementsByClassName("h2");
   for (var i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
